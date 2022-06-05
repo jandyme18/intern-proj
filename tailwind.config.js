8f8010d296337555bfe7a75f50341338488a5521
jandyme18 <69596572+jandyme18@users.noreply.github.com>
2022-06-05 23:48:21 +0700

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-blue': '#003265',
+        'blue': '#004489',
+        'light-brown': '#D4B139',
+        'brown': '#B9870C',
+        'white-malt':'#FFFEFA'
+      },
+    },
   },
   plugins: [],
 }
